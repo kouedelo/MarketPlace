fcf16c1f89509d7c0749fc8c641ae307ea121861
Kouede Loic <kouedelo@gmail.com>
2022-07-06 15:38:03 -0400

    debugged app

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let x = 1;
+
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>
