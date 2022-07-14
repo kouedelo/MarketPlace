b8c5ea7030dea42d427c4f935fc5465396a76606
Kouede Loic <kouedelo@gmail.com>
2022-07-14 05:33:01 -0400

    testing expo

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
+import {
+  useDeviceOrientation,
+  useDimensions,
+} from "@react-native-community/hooks";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   let x = 1;
 
+  const { landscape } = useDeviceOrientation();
+
   console.log("App executed");
 
+  console.log(Dimensions.get("screen"));
+
+  console.log(useDimensions());
+
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
+    <View>
       <StatusBar style="auto" />
     </View>
   );
