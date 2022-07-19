2c40a846b5a0498891c4bc6d9149986451ff55c5
Kouede Loic <kouedelo@gmail.com>
2022-07-19 16:53:40 -0400

    added padding top to the messages screen

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import ListItem from "../components/ListItem";
+import Constants from "expo-constants";
 
 const messages = [
   {
@@ -9,22 +10,34 @@ const messages = [
     description: "D1",
     image: require("../assets/random_person.jpg"),
   },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/random_person.jpg"),
+  },
 ];
 
 function MessagesScreen(props) {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-        />
-      )}
-    />
+    <SafeAreaView style={styles.screen}>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 export default MessagesScreen;
+
+const styles = StyleSheet.create({
+  screen: { paddingTop: Constants.statusBarHeight },
+});
