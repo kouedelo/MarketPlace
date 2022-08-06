9b4a5eb4c9aaff92c983dad47b768476e5dcce11
Kouede Loic <kouedelo@gmail.com>
2022-08-06 10:51:12 -0400

    extending list item component to support icons

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -13,16 +13,26 @@ import {
   Swipeable,
 } from "react-native-gesture-handler";
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  ImageComponent,
+  image,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <GestureHandlerRootView>
       <Swipeable renderRightActions={renderRightActions}>
         <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
           <View style={styles.container}>
-            <Image style={styles.image} source={image} />
-            <View>
+            {ImageComponent}
+            {image && <Image style={styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
               <AppText style={styles.title}>{title} </AppText>
-              <AppText style={styles.subTitle}>{subTitle} </AppText>
+              {subTitle && (
+                <AppText style={styles.subTitle}>{subTitle} </AppText>
+              )}
             </View>
           </View>
         </TouchableHighlight>
@@ -35,7 +45,8 @@ export default ListItem;
 
 const styles = StyleSheet.create({
   container: { flexDirection: "row", padding: 15 },
-  image: { width: 70, height: 70, borderRadius: 35, marginRight: 10 },
+  detailsContainer: { marginLeft: 10, justifyContent: "center" },
+  image: { width: 70, height: 70, borderRadius: 35 },
   title: { fontWeight: "500" },
   subTitle: { color: colors.medium },
 });
