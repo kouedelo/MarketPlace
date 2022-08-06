cd966c898185232e49b6e7a7ae63a6f191dd1d0b
Kouede Loic <kouedelo@gmail.com>
2022-08-06 14:03:48 -0400

    implemented custom text input component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import {} from "@react-native-community/hooks";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
@@ -9,10 +10,17 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from './app/screens/ListingsScreen';
+import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
