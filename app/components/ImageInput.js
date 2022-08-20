0ab9f0919d6f7984f4b5976733c20d30220c925e
Kouede Loic <kouedelo@gmail.com>
2022-08-19 21:50:30 -0400

    implementing touch on image input component

--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,17 +1,64 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
+import * as ImagePicker from "expo-image-picker";
 
-function ImageInput({ imageUri }) {
+function ImageInput({ imageUri, onChangeImage }) {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    //  const {granted}= Permissions.askAsync(Permissions.CAMERA);
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) {
+      alert("You need to enable permission to access the library.");
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        onChangeImage(result.uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  const handlePress = async () => {
+    if (!imageUri) {
+      selectImage();
+    } else {
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri ? (
-        <MaterialCommunityIcons color={colors.medium} name="camera" size={40} />
-      ) : (
-        <Image source={{ uri: imageUri }} style={styles.image} />
-      )}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri ? (
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="camera"
+            size={40}
+          />
+        ) : (
+          <Image source={{ uri: imageUri }} style={styles.image} />
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
